967c43349c1f924c3ee45816b186499e3c5788d4
fredy <devfredylgnt@gmail.com>
2025-08-27 13:30:04 +0300

    V 1.0.12

--- scripts/script.js
+++ scripts/script.js
@@ -1,345 +1,324 @@
 let scoresTotal = 0;
 
-let gameHistory = JSON.parse(localStorage.getItem('gamehistory'))  || []
+let gameHistory = JSON.parse(localStorage.getItem("gamehistory")) || [];
 
 let scores = {
-    wins: 0,
-    loses: 0,
-    ties: 0
-}
+  wins: 0,
+  loses: 0,
+  ties: 0,
+};
 
+document.querySelector(".js_rock_button").addEventListener("click", () => {
+  playGame("rock");
+});
 
-document.querySelector('.js_rock_button').addEventListener('click', () => {
+document.querySelector(".js_scissors_button").addEventListener("click", () => {
+  playGame("scissors");
+});
 
-    playGame('rock')
-
-
-    
-})
-
-document.querySelector('.js_scissors_button').addEventListener('click', () => {
-    playGame('scissors')
-})
-
-document.querySelector('.js_paper_button').addEventListener('click', () => {
-    playGame('paper')
-})
+document.querySelector(".js_paper_button").addEventListener("click", () => {
+  playGame("paper");
+});
 
 function renderHistorySectionTitle() {
-    if (gameHistory.length < 1) {
-    document.querySelector('.history_part').innerHTML = ''
-} else {
-    document.querySelector('.history_part').innerHTML = '<h1>History</h1>'
-}
+  if (gameHistory.length < 1) {
+    document.querySelector(".history_part").innerHTML = "";
+  } else {
+    document.querySelector(".history_part").innerHTML = "<h1>History</h1>";
+  }
 }
 
-renderHistorySectionTitle()
+renderHistorySectionTitle();
 
 let playGame = (playerMove) => {
-    let computerMove = pickComputerMove();
+  let computerMove = pickComputerMove();
 
-    document.querySelector('.js_choices').innerHTML = `You chose
+  document.querySelector(".js_choices").innerHTML = `You chose
                 <img src="./utils/images/${playerMove}.webp" alt=""> and computer chose
-                <img src="./utils/images/${computerMove}.webp" alt="">`
+                <img src="./utils/images/${computerMove}.webp" alt="">`;
 
-    let result = ''
+  let result = "";
 
-    if (playerMove === 'rock') {
-        if (computerMove === 'rock') {
-            result = 'tie'
-        } else if (computerMove === 'paper') {
-            result = 'lose'
-        } else if (computerMove === 'scissors') {
-            result = 'win'
-        }
-    } else if (playerMove === 'paper') {
-        if (computerMove === 'rock') {
-            result = 'win'
-        } else if (computerMove === 'paper') {
-            result = 'tie' 
-        } else if (computerMove === 'scissors') {
-            result = 'lose'
-        }
-    } else if (playerMove === 'scissors') {
-        if (computerMove === 'rock') {
-            result = 'lose'
-        } else if (computerMove === 'paper') {
-            result = 'win'
-        } else if (computerMove === 'scissors') {
-            result = 'tie'
-        }
+  if (playerMove === "rock") {
+    if (computerMove === "rock") {
+      result = "tie";
+    } else if (computerMove === "paper") {
+      result = "lose";
+    } else if (computerMove === "scissors") {
+      result = "win";
+    }
+  } else if (playerMove === "paper") {
+    if (computerMove === "rock") {
+      result = "win";
+    } else if (computerMove === "paper") {
+      result = "tie";
+    } else if (computerMove === "scissors") {
+      result = "lose";
+    }
+  } else if (playerMove === "scissors") {
+    if (computerMove === "rock") {
+      result = "lose";
+    } else if (computerMove === "paper") {
+      result = "win";
+    } else if (computerMove === "scissors") {
+      result = "tie";
+    }
+  }
+
+  document.querySelector(".js_results").innerHTML = result;
+
+  if (result === "win") {
+    scores.wins++;
+    scoresTotal++;
+  } else if (result === "lose") {
+    scores.loses++;
+    scoresTotal++;
+  } else if (result === "tie") {
+    scores.ties++;
+    scoresTotal++;
+  }
+
+  if (document.querySelector(".js_results").innerText === "WIN") {
+    document.querySelector(".js_results").classList.add("wins_text");
+    document.querySelector(".js_results").classList.remove("loses_text");
+    document.querySelector(".js_results").classList.remove("ties_text");
+  } else if (document.querySelector(".js_results").innerText === "LOSE") {
+    document.querySelector(".js_results").classList.remove("wins_text");
+    document.querySelector(".js_results").classList.remove("ties_text");
+    document.querySelector(".js_results").classList.add("loses_text");
+  } else if (document.querySelector(".js_results").innerText === "TIE") {
+    document.querySelector(".js_results").classList.remove("wins_text");
+    document.querySelector(".js_results").classList.remove("loses_text");
+    document.querySelector(".js_results").classList.add("ties_text");
+  }
+
+  renderScores();
+
+  if (scoresTotal === 10) {
+    let final = "";
+    if (scores.wins > scores.loses) {
+      final = "You win against computer";
+    } else if (scores.loses > scores.wins) {
+      final = "You lose to computer";
+    } else {
+      final = "You tied with computer";
     }
 
-    document.querySelector('.js_results').innerHTML = result;
+    let historyDate = getDayTime();
 
-    if (result === 'win') {
-        scores.wins++;
-        scoresTotal++
-    } else if (result === 'lose') {
-        scores.loses++;
-        scoresTotal++
-    } else if (result === 'tie') {
-        scores.ties++;
-        scoresTotal++
-    }
+    gameHistory.push({
+      final,
+      wins: scores.wins,
+      ties: scores.ties,
+      loses: scores.loses,
+      historyDate,
+    });
 
-    document.querySelector('.js_scores').innerHTML = `Wins: ${scores.wins} Ties: ${scores.ties} Loses: ${scores.loses}`
-    
-    
-    if (scoresTotal === 10) {
-        let final = ''
-        if (scores.wins > scores.loses) {
+    console.log(historyDate);
 
-            
-            
-            final = 'You win against computer'
-        } else if (scores.loses > scores.wins) {
-            final = 'You lose to computer'
-        } else {
-            final = 'You tied with computer'
-        }
+    saveGameHistory();
 
-        let historyDate = getDayTime();
+    alert(final);
+    renderHistory();
 
-        gameHistory.push(
-            {
-                final,
-                wins: scores.wins,
-                ties: scores.ties,
-                loses: scores.loses,
-historyDate
-            }
-        )
+    scoresTotal = 0;
 
-        console.log(historyDate)
+    scores.loses = 0;
+    scores.wins = 0;
+    scores.ties = 0;
 
-        saveGameHistory()
+    renderScores();
 
-        alert(final)
+    renderHistorySectionTitle();
 
-        
-
-        scoresTotal = 0
-
-        scores.loses = 0
-        scores.wins = 0
-        scores.ties = 0
-        
-        renderScores()
-
-        renderHistorySectionTitle()
-        
-
-        renderHistory()
-        saveGameHistory()
-        
-    }  
-    
-
-
-   
-    
-}
+    saveGameHistory();
+  }
+};
 
 let endNumber = 10;
 
 let renderHistory = () => {
-    let historyHtml = ''
+  let historyHtml = "";
 
-    
+  let newGameHistory = gameHistory.slice(0, endNumber);
 
-    let newGameHistory = gameHistory.slice(0, endNumber);
+  document.querySelector(".js_more_button").addEventListener("click", () => {
+    endNumber = endNumber + 10;
+    renderHistory();
+  });
 
-    document.querySelector('.js_more_button').addEventListener('click', () => {
-        
-        endNumber = endNumber + 10
-        renderHistory()
-    })
+  if (endNumber > gameHistory.length) {
+    document.querySelector(".js_more_button").innerHTML = "";
+  }
 
-    if (endNumber > gameHistory.length) {
-        document.querySelector('.js_more_button').innerHTML = ''
-    }
-
-    newGameHistory.map((value, index) => {
-        let html = ` <div class='show_history'>
+  newGameHistory.map((value, index) => {
+    let html = ` <div class='show_history'>
         <span class='history_num'>
-        ${index + 1}
+        ${index + 1}. 
         </span>
         <div class='history_final'>
         ${value.final}
         </div>
         <div class='history_scores'>
         
-         <span> Wins: 
+         <span class='wins_text'> Wins: 
         ${value.wins}
-        </span> <span>
+        </span> <span class='ties_text'>
         Ties: 
         ${value.ties}
-        </span> <span>
+        </span> <span class='loses_text'>
         Loses: 
         ${value.loses}
         </span>
-        </div> <button class='js_delete_history'>
-        Delete</button></div>`
-        historyHtml += html
-    })
-    // console.log(historyHtml)
-    document.querySelector('.js_history').innerHTML = historyHtml
+        </div> <button class='js_delete_history delete_history'>
+        Delete</button></div>`;
+    historyHtml += html;
+  });
+  // console.log(historyHtml)
+  document.querySelector(".js_history").innerHTML = historyHtml;
 
-    document.querySelectorAll('.js_delete_history').forEach((button, index) => {
+  document.querySelectorAll(".js_delete_history").forEach((button, index) => {
+    button.addEventListener("click", () => {
+      gameHistory.splice(index, 1);
+      renderHistory();
+      saveGameHistory();
+      renderHistorySectionTitle();
+    });
+  });
+};
 
-        button.addEventListener('click', () => {
-            gameHistory.splice(index, 1)
-            renderHistory()
-            saveGameHistory()
-            renderHistorySectionTitle()
-        })
-    })
-    
-}
-
-renderHistory()
+renderHistory();
 
 let saveGameHistory = () => {
-    localStorage.setItem('gamehistory', JSON.stringify(gameHistory))
-}
+  localStorage.setItem("gamehistory", JSON.stringify(gameHistory));
+};
 
-
-renderScores()
+renderScores();
 
 function renderScores() {
-    document.querySelector('.js_scores').innerHTML = `Wins: ${scores.wins} Ties: ${scores.ties} Loses: ${scores.loses}`
-    
+  document.querySelector(
+    ".js_scores"
+  ).innerHTML = `<span class='wins_text'>Wins: ${scores.wins}</span> <span class='ties_text'>Ties: ${scores.ties}</span> <span class='loses_text'>
+  Loses: ${scores.loses}</span>`;
 }
 
 function pickComputerMove() {
-    let randomNumber = Math.random();
+  let randomNumber = Math.random();
 
-    let computerMove = ''
+  let computerMove = "";
 
-    if (randomNumber >= 0 & randomNumber < 1 / 3) {
-        computerMove = 'rock';
-    } else if (randomNumber >= 1 / 3 & randomNumber < 2 / 3) {
-        computerMove = 'paper';
-    } else if (randomNumber >= 2 / 3 & randomNumber < 3 / 3) {
-        computerMove = 'scissors'
-    }
+  if ((randomNumber >= 0) & (randomNumber < 1 / 3)) {
+    computerMove = "rock";
+  } else if ((randomNumber >= 1 / 3) & (randomNumber < 2 / 3)) {
+    computerMove = "paper";
+  } else if ((randomNumber >= 2 / 3) & (randomNumber < 3 / 3)) {
+    computerMove = "scissors";
+  }
 
-    return computerMove;
-
+  return computerMove;
 }
 
-document.querySelector('.js_reset').addEventListener('click', (
-    
-) => {
-    document.querySelector('.js_reset_text').classList.remove('reset_text')
-})
+document.querySelector(".js_reset").addEventListener("click", () => {
+  document.querySelector(".js_reset_text").classList.remove("reset_text");
+});
 
+document.querySelector(".js_yes_reset").addEventListener("click", () => {
+  scores.loses = 0;
+  scores.wins = 0;
+  scores.ties = 0;
 
-document.querySelector('.js_yes_reset').addEventListener('click', () => {
-    scores.loses = 0
-        scores.wins = 0
-        scores.ties = 0
+  document.querySelector(".js_results").innerHTML = "";
 
-        document.querySelector('.js_results').innerHTML = '';
+  scoresTotal = 0;
 
-        scoresTotal = 0
+  renderScores();
 
-        renderScores()
+  document.querySelector(".js_choices").innerHTML = "";
 
-        document.querySelector('.js_reset_text').classList.add('reset_text')
-})
+  document.querySelector(".js_reset_text").classList.add("reset_text");
+});
 
-document.querySelector('.js_no_reset').addEventListener('click', () => {
-    document.querySelector('.js_reset_text').classList.add('reset_text')
-})
+document.querySelector(".js_no_reset").addEventListener("click", () => {
+  document.querySelector(".js_reset_text").classList.add("reset_text");
+});
 
-let actualDay = ''
+let actualDay = "";
 
 let getDayTime = () => {
-    let date = new Date().getDate();
-    let day = new Date().getDay();
-    let thisMonth = new Date().getMonth() + 1
-    let dayTimeHours = new Date().getHours()
-    let dayTimeMinutes = new Date().getMinutes();
-    let dayTimeSeconds = new Date().getSeconds();
+  let date = new Date().getDate();
+  let day = new Date().getDay();
+  let thisMonth = new Date().getMonth() + 1;
+  let dayTimeHours = new Date().getHours();
+  let dayTimeMinutes = new Date().getMinutes();
+  let dayTimeSeconds = new Date().getSeconds();
 
-    if (day === 1) {
-        actualDay = 'Monday'
-    } else if (day === 2) {
-        actualDay = 'Tuesday'
-    } else if (day === 3) {
-        actualDay = 'Wednesday'
-    } else if (day === 4) {
-        actualDay = 'Thursday'
-    } else if (day === 5) {
-        actualDay = 'Friday'
-    } else if (day === 6) {
-        actualDay = 'Saturday'
-    } else if (day === 7) {
-        actualDay = 'Sunday'
-    }
+  if (day === 1) {
+    actualDay = "Monday";
+  } else if (day === 2) {
+    actualDay = "Tuesday";
+  } else if (day === 3) {
+    actualDay = "Wednesday";
+  } else if (day === 4) {
+    actualDay = "Thursday";
+  } else if (day === 5) {
+    actualDay = "Friday";
+  } else if (day === 6) {
+    actualDay = "Saturday";
+  } else if (day === 7) {
+    actualDay = "Sunday";
+  }
 
-    let fulldate = 
+  let fulldate = {
+    date,
+    thisMonth,
+    actualDay,
+    dayTimeHours,
+    dayTimeMinutes,
+    dayTimeSeconds,
+  };
 
-    {
-        date, thisMonth, actualDay, dayTimeHours, dayTimeMinutes, dayTimeSeconds
-    }
+  return fulldate;
+};
 
-
-
-    return fulldate;
-}
-
-console.log(getDayTime())
-
-
+console.log(getDayTime());
 
 let getDate = () => {
-    let currYear = new Date().getFullYear()
-    return currYear;
-}
+  let currYear = new Date().getFullYear();
+  return currYear;
+};
 
-document.querySelector('.js_footer').innerHTML = `Copyright ${' '} &copy; ${' '}<span class="js_date"></span>${'   '} | All Rights Reserved.`
+document.querySelector(
+  ".js_footer"
+).innerHTML = `Copyright ${" "} &copy; ${" "}<span class="js_date"></span>${"   "} | All Rights Reserved.`;
 
-document.querySelector('.js_date').innerHTML = getDate()
+document.querySelector(".js_date").innerHTML = getDate();
 
-console.log(scoresTotal)
-
-
-
-
+console.log(scoresTotal);
 
 let isAutoPlay = false;
 let playInterval;
 
-document.querySelector('.js_auto_play').addEventListener('click', (
-    
-) => {
-    auto_play()
-})
-
-
+document.querySelector(".js_auto_play").addEventListener("click", () => {
+  auto_play();
+});
 
 function auto_play() {
-    
-    
-    if (!isAutoPlay) {
-        playInterval = setInterval(() => {
-            let autoMove = pickComputerMove()
-        playGame(autoMove);
-        
-        console.log(playInterval)
+  if (!isAutoPlay) {
+    playInterval = setInterval(() => {
+      let autoMove = pickComputerMove();
+      playGame(autoMove);
 
-        document.querySelector('.js_auto_play').innerText = 'Stop'
-        }, 1000)
-isAutoPlay = true
-        
-        
-    } else {
-        clearInterval(playInterval)
-        console.log(playInterval)
-        document.querySelector('.js_auto_play').innerText = 'Auto Play'
-        isAutoPlay = false
-    }
+      console.log(playInterval);
+
+      document.querySelector(".js_auto_play").innerText = "Stop";
+      document.querySelector(".js_auto_play").classList.add("is_auto_play");
+    }, 2000);
+    isAutoPlay = true;
+  } else {
+    clearInterval(playInterval);
+    console.log(playInterval);
+    document.querySelector(".js_auto_play").innerText = "Auto Play";
+    document.querySelector(".js_auto_play").classList.remove("is_auto_play");
+    isAutoPlay = false;
+  }
 }
